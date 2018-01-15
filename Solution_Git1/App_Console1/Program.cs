e6d89dd90b2376590c50cb4cf06a6fcb3fff12be
studenti <studenti@LAB2-PC-20>
2018-01-15 09:49:16 +0100

    prima commit

---
+++ Solution_Git1/App_Console1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App_Console1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ciao");
+            Console.WriteLine("Ho fatto una modifica");
+            Console.ReadLine();
+        }
+    }
+}
